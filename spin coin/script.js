e98d2a9e1c4c0b33d515340072321fb1ab5db53b
viswakiran1 <viswa.kr@volkotech.in>
2021-05-13 10:25:23 +0530

    storage

--- spin coin/script.js
+++ spin coin/script.js
@@ -1,80 +1,130 @@
+'use strict'
+
+const startNew = document.getElementById('new');
+const headorTailsSpin = document.getElementById('coin');
+const spinButton = document.getElementById('spin');
+
+const playerName = document.getElementById('player-name');
+const playerbetMoney = document.getElementById('bet-money');
+const playerselectOption = document.getElementById('select');
+const buttonAdd = document.getElementById('btn-add');
+const listAdd = document.querySelector('.list-add')
 
 
+var currentBet = 0;
+var currentBet1 = 0;
+var totalMoney = 0;
+var wonMoney = 0;
+var headorTailsSpinEl;
 
-const coinel=document.querySelector(".coin");
-const button=document.querySelector("#button");
-const diplay=document.querySelector("#display");
-const total=document.querySelector("#total");
-
-
-const heads=document.getElementById("heads");
-const spin=document.getElementById("spin");
-spin.addEventListener("click",function(){
-    let coins=Math.trunc(Math.random() * 2) 
-    let coin=["H","T"];
-    console.log(coin[`${coins}`]);
+spinButton.addEventListener('click', () => {
+    let coin = ['H', 'T'];
+    headorTailsSpinEl = Math.trunc(Math.random() * 2);
+    document.querySelector('.progresssteps').innerHTML = 'Spinning in progress...Wait for 3 seconds'
     setTimeout(() => {
-    document.getElementById("heads").textContent=coin[`${coins}`]
-    document.getElementById("heads").style.fontSize="60px";
-}, 3000),
-
-document.getElementById("show1").textContent = 'Spinning in progress...Wait for 3 seconds'
-setTimeout(() => {
-             document.getElementById('show1').style.display = "none";
-             document.getElementById('show').textContent = 'game is in progress'
-     }, 3000);
         
+        document.getElementById('coin').textContent = coin[`${headorTailsSpinEl}`]
+        
+    }, 3000);
+    setTimeout(() => {
+            document.querySelector('.progresssteps').style.display = "none";
+            document.getElementById('contest').textContent = 'game is in progress'
+            
+            
+        }, 3000);
+    setTimeout( () => {
+        winner();
+    },5000)
+    localStorage.setItem('headorTailsSpinEl', headorTailsSpinEl);
+    
+
 });
-var todo=document.querySelector("#todo");
-var bet=document.querySelector("#bet");
-var choose=document.querySelector("#selecters");
-var additems=document.querySelector("#submit");
-var items=JSON.parse(localStorage.getItem("heads or tails")) || [];
-additems.addEventListener("click",function(){
-    additems()
-function selectitems(){
+
+buttonAdd.addEventListener('click', () => {
+    addItem()
+
+})
+
+
+function select() {
+    if (document.getElementById('val1').selected) {
+        currentBet += Number(playerbetMoney.value)
+        console.log(currentBet)
+    } else if (document.getElementById('val2').selected) {
+        currentBet1 += Number(playerbetMoney.value)
+    }
+}
+
+var items = JSON.parse(localStorage.getItem('head-or-tails')) || [];
+
+function addItem() {
+    // item = inputBox.value;
+    // inputBox = document.getElementById('player-name')
+    var playerName = document.getElementById('player-name');
+    var item = playerName.value;
+    var item1 = playerbetMoney.value
+    var item2 = playerselectOption.value
+    winner()
+
+
+    if (item === '') {
+        return document.getElementById('list').innerHTML = "you need to add name and bet";
+    }
+    items.push({
+        playerName: item,
+        playerbetMoney: item1,
+        playerselectOption: item2
+    })
+    localStorage.setItem('head-or-tails', JSON.stringify(items));
+    listItems()
+    // inputBox.value = ''
+    playerName.value = ''
+    playerbetMoney.value = ''
+    playerselectOption.value = ''
+}
+
+function listItems() {
+    var list = ''
+
+    for (var i = 0; i < items.length; i++) {
+        list += "<li>"
+        list += items[i].value + " "
+
+    }
+    document.getElementById('list').innerHTML = ''
+    const add = document.createElement('li')
+    add.innerHTML = `${playerName.value} , ${playerbetMoney.value} : ${playerselectOption.value}`
+    listAdd.appendChild(add);
+    select()
+    document.querySelector('.heads-total').innerHTML = `Total bets in heads ${currentBet}`
+    document.querySelector('.tails-total').innerHTML = `Total bets in tails ${currentBet1}`
+
+
 
 }
 
-function additems(){
-    var  item=todo.value;
-    var  item1=bet.value;
-    var  item2=choose.value;
-    if(item===""){
-        return document.querySelector("#empty").textContent="you need to enter  name ";
-    }items.push({
-      todo: item,
-      bet: item1,
-      choose: item2
+function winner() {
+
+    // const headorTailsSpinEl = Math.trunc(Math.random() * 2) + 1;
+    setTimeout(() => {
        
-    })
-    localStorage.setItem('todo-list',JSON.stringify(items));
-    listItems();
-    todo.value="";
+        if (headorTailsSpinEl === 0) {
+            document.getElementById('contest').innerHTML = ''
+            document.getElementById('list').innerHTML = ''
+            const add = document.createElement('li')
+            add.innerHTML = `${playerName.value} , ${totalMoney += currentBet * 2} Won(${wonMoney += currentBet})`
+            document.getElementById('contest').appendChild(add);
 
- }
- function deleteItem(index){
-     items.splice(index,1);
-     localStorage.setItem('todo-list', JSON.stringify(items))
-         listItems();
- }
- function markAsDone(index) {
-     items[index].done = !items[index].done;
-     localStorage.setItem('todo-list', JSON.stringify(items));
-     listItems();
- }
- function listItems() {
-     var list = "";
-     for (var i = 0; i < items.length; i++) {
-         list += "<li>";
-         list += items[i].value + " ";
-        list += "<small title='click me to mark as done' class='label' onclick='markAsDone(" + i + ")'>" + items[i].time + "</small> ";
-         list += "<span class='label label-1 alert' onclick='deleteItem(" + i + ")'>Remove</span></li>";
+        } else if (headorTailsSpinEl === 1) {
+            document.getElementById('contest').innerHTML = ''
+            document.getElementById('list').innerHTML = ''
+            const add = document.createElement('li')
+            add.innerHTML = `${playerName.value} , ${totalMoney += currentBet1 * 2} Won(${wonMoney += currentBet1})`
+            document.getElementById('contest').appendChild(add);
+        }
+    }, 3)
+}
 
-     }
-     document.querySelector("#list-items").innerHTML = list;
- }
- (function() {
-     listItems();
- })();
-});
+startNew.addEventListener('click', () => {
+    location.reload()
+}) 
